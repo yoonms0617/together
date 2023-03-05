857ac5838d3fe65aba344c50995e6a5c6c343c64
Yoon <yoonminwater@gmail.com>
2023-03-05 21:04:07 +0900

    chore: 프로젝트 구조 변경 (#10)
    * chore: spring security, apache tiles 의존성 추가
    
    * chore: 정적 파일 경로 수정, message converter 추가
    
    * style: 경로 수정
    
    * feat: 시큐리티 설정 추가
    
    * feat: 메인 컨트롤러 추가

---
+++ project/src/main/java/com/sist/security/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.sist.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .authorizeHttpRequests(request -> request
+                        .antMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().antMatchers("/css/**", "/js/**", "/image/**");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+
+}
