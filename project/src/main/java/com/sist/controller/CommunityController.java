39cd44139c94af4ec378a60c8ffab49036237d0e
songsangeun <121929133+songsangeun@users.noreply.github.com>
2023-03-09 22:18:16 +0900

    feat: 커뮤니티 vo, mapper, service 추가 (#20)

---
+++ project/src/main/java/com/sist/controller/CommunityController.java
@@ -0,0 +1,13 @@
+package com.sist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CommunityController {
+	@GetMapping("community/list.do")
+	public String community_list()
+	{
+		return "community/list";
+	}
+}
