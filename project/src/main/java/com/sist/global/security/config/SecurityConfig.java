8ca2981e86c03f46bd324e81372c02468f0a4966
yoonms0617 <yoonminwater@gmail.com>
2023-03-12 19:26:25 +0900

    feat: 로그인 설정 추가

--- project/src/main/java/com/sist/global/security/config/SecurityConfig.java
+++ project/src/main/java/com/sist/global/security/config/SecurityConfig.java
@@ -1,20 +1,42 @@
 package com.sist.global.security.config;
 
+import com.sist.global.security.handler.LoginFailureHandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.httpBasic().disable();
-        http.csrf().disable();
+        http
+                .httpBasic().disable()
+                .csrf().disable();
+        http.formLogin(form -> form
+                .loginProcessingUrl("/member/login")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .failureHandler(loginFailureHandler())
+                .defaultSuccessUrl("/")
+        );
+        http.logout(logout -> logout
+                .logoutUrl("/member/logout")
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+                .logoutSuccessUrl("/")
+        );
         http.authorizeHttpRequests(request -> request
                 .anyRequest().permitAll()
         );
@@ -31,5 +53,9 @@ public class SecurityConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationFailureHandler loginFailureHandler() {
+        return new LoginFailureHandler();
+    }
 
 }
